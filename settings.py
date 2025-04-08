fd29c0b42a2ce925ed49097648561f1e7e4c2eb0
Kie Sato <kie.1993@icloud.com>
2025-04-08 09:56:44 +0900

    fix: Postgre移行のコミット漏れ

--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
-path = 'sqlite:///db.sqlite3'
+load_dotenv()
+
+path = os.getenv("POSTGRE_DATABASE_URL")
 
 Base = declarative_base()
